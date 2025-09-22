a89d10dd20e89846665e322a99561f6b92f12c9b
Baykov Aleksandr <baykov-1988@mail.ru>
2025-09-22 23:08:15 +0300

    add react vite template and base jsx

---
+++ webapp/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Ideanick</h1>
+      <h2>Idea 1</h2>
+      <p>Description of idea 1...</p>
+      <div>
+        <h2>Idea 2</h2>
+        <p>Description of idea 2...</p>
+      </div>
+      <div>
+        <h2>Idea 3</h2>
+        <p>Description of idea 3...</p>
+      </div>
+    </div>
+  );
+};
