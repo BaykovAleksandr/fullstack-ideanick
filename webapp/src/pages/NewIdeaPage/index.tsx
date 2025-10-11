898afb456c3f4e91481c8f1b3c52d0592cb6ceb0
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-11 15:53:15 +0300

    add newIdeaPage

---
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -0,0 +1,12 @@
+
+import css from './index.module.scss'
+
+
+export const NewIdeaPage = () => {
+  return (
+	<div>
+		<h1 className={css.title}>NewIdea</h1>
+		<div>Form will be here</div>
+	</div>
+  )
+}
