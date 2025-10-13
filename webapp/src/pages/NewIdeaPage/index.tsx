bdfa936e3a00f22e92ec9ad2e803de0cd17f0817
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-13 22:59:10 +0300

    add zod for form validation

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,7 +1,19 @@
+import { toFormikValidationSchema } from 'zod-formik-adapter';
 import { Input } from '../../components/Input';
 import { Segment } from '../../components/Segment';
 import { Textarea } from '../../components/Textarea';
 import { useFormik } from 'formik';
+import { z } from 'zod';
+
+const validationSchema = z.object({
+  name: z.string().min(1),
+  nick: z
+    .string()
+    .min(1)
+    .regex(/^[a-z0-9-]+$/, 'Nick may contain only lowercase letters, numbers and dashes'),
+  description: z.string().min(1),
+  text: z.string().min(100, 'Text should be at least 100 characters long'),
+});
 
 export const NewIdeaPage = () => {
   const formik = useFormik({
@@ -11,26 +23,7 @@ export const NewIdeaPage = () => {
       description: '',
       text: '',
     },
-    validate: (values) => {
-      const errors: Partial<typeof values> = {};
-      if (!values.name) {
-        errors.name = 'Name is required';
-      }
-      if (!values.nick) {
-        errors.nick = 'Nick is required';
-      } else if (!values.nick.match(/^[a-z0-9-]+$/)) {
-        errors.nick = 'Nick may contain only lowercase letters, numbers and dashes';
-      }
-      if (!values.description) {
-        errors.description = 'Description is required';
-      }
-      if (!values.text) {
-        errors.text = 'Text is required';
-      } else if (values.text.length < 100) {
-        errors.text = 'Text should be at least 100 characters long';
-      }
-      return errors;
-    },
+    validationSchema: toFormikValidationSchema(validationSchema),
     onSubmit: (values) => {
       console.info('Submitted', values);
     },
