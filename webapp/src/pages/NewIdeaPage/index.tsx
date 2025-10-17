06e2dc685344c41fdfe19026056b8de07d8b71fd
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-17 19:10:43 +0300

    Share mutation input zod schema

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -3,18 +3,8 @@ import { Input } from '../../components/Input';
 import { Segment } from '../../components/Segment';
 import { Textarea } from '../../components/Textarea';
 import { useFormik } from 'formik';
-import { z } from 'zod';
+import { zCreateIdeaTrpcInput } from '@ideanick/backend/src/router/createIdea/input';
 import { trpc } from '../../lib/trpc';
-
-const validationSchema = z.object({
-  name: z.string().min(1),
-  nick: z
-    .string()
-    .min(1)
-    .regex(/^[a-z0-9-]+$/, 'Nick may contain only lowercase letters, numbers and dashes'),
-  description: z.string().min(1),
-  text: z.string().min(100, 'Text should be at least 100 characters long'),
-});
 
 export const NewIdeaPage = () => {
   const createIdea = trpc.createIdea.useMutation();
@@ -25,7 +15,7 @@ export const NewIdeaPage = () => {
       description: '',
       text: '',
     },
-    validationSchema: toFormikValidationSchema(validationSchema),
+    validationSchema: toFormikValidationSchema(zCreateIdeaTrpcInput),
     onSubmit: async (values) => {
       await createIdea.mutateAsync(values);
     },
