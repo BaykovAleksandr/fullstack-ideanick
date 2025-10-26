d1c73bdb93251a78e998b2a7d72ffb24df6be006
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-26 21:00:56 +0300

    Add superjson

--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -2,7 +2,9 @@ import type { TrpcRouter } from '@ideanick/backend/src/router';
 import { createTRPCReact } from '@trpc/react-query';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
+import superjson from 'superjson';
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const trpc = createTRPCReact<TrpcRouter>();
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,6 +19,7 @@ const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
       url: 'http://localhost:3000/trpc',
+      transformer: superjson,
     }),
   ],
 });
