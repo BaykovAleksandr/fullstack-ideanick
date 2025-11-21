c903162147e6d76c368cb3cb63f2ca8805fb33de
Baykov Aleksandr <baykov-1988@mail.ru>
2025-11-21 21:44:42 +0300

    Add search feature

--- backend/src/router/ideas/getIdeas/index.ts
+++ backend/src/router/ideas/getIdeas/index.ts
@@ -16,6 +16,30 @@ export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcInput).query(
         },
       },
     },
+    where: !input.search
+      ? undefined
+      : {
+          OR: [
+            {
+              name: {
+                contains: input.search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              description: {
+                contains: input.search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              text: {
+                contains: input.search,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        },
     orderBy: [
       {
         createdAt: 'desc',
@@ -27,6 +51,7 @@ export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcInput).query(
     cursor: input.cursor ? { serialNumber: input.cursor } : undefined,
     take: input.limit + 1,
   });
+
   const nextIdea = rawIdeas.at(input.limit);
   const nextCursor = nextIdea?.serialNumber;
   const rawIdeasExceptNext = rawIdeas.slice(0, input.limit);
@@ -34,5 +59,6 @@ export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcInput).query(
     ..._.omit(idea, ['_count']),
     likesCount: idea._count.ideasLikes,
   }));
-  return { ideas: ideasExceptNext, nextCursor }
+
+  return { ideas: ideasExceptNext, nextCursor };
 });
